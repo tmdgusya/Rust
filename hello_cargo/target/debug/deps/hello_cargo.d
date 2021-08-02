56547274e7e52ea0ffcb6cb6fc8211922bb1b37c
roach <roach@cleaninglab.co.kr>
2021-08-02 19:41:50 +0900

    Cargo project init

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/jeongseunghyeon/Desktop/study/Rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/jeongseunghyeon/Desktop/study/Rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/jeongseunghyeon/Desktop/study/Rust/hello_cargo/target/debug/hello_cargo: /Users/jeongseunghyeon/Desktop/study/Rust/hello_cargo/src/main.rs
