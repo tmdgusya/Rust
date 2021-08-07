08f9c84a43d71fe1f324cb341d317769f2283852
roach <tmdgusya@naver.com>
2021-08-08 01:14:16 +0900

    refactore: using struct

--- struct/question.rs
+++ struct/question.rs
@@ -1,13 +1,18 @@
-fn main() {
+struct Rectangle {
+    length: u32,
+    width: u32
+}
 
-    let rect1 = (50, 30);
+
+fn main() {
+    let rect1 = Rectangle { length: 50, width: 30};
 
     println!(
         "The area of the rectangle is {} square pixels", 
-        area(rect1)
+        area(&rect1)
     )
 }
 
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
 }
