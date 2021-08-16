a157c3c2371b5c90a1516428b8027849e8143359
roach <tmdgusya@naver.com>
2021-08-16 22:55:22 +0900

    refactor using IpAddrKind

--- enum_sample/src/main.rs
+++ enum_sample/src/main.rs
@@ -1,6 +1,6 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 struct IpAddr {
@@ -8,15 +8,9 @@ struct IpAddr {
     address: String,
 }
 
-let home = IpAddr {
-    kind: IpAddrKind::V4,
-    address: String::from("127.0.0.1"),
-}
+let home = IpAddr::V4(String::from("127.0.0.1"))
 
-let loopback = IpAddr {
-    kind: IpAddrKind::V6,
-    address: String::from("::1"),
-}
+let loopback = IpAddr::V4(String::from("::1"));
 
 fn main() {
     println!("Hello, world!");
