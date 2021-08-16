237073c6142186e91ed92a17b874770dc0af3aad
roach <tmdgusya@naver.com>
2021-08-16 23:23:32 +0900

    match 를 활용1

--- match_example/src/main.rs
+++ match_example/src/main.rs
@@ -1,3 +1,35 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    // match can comparison any type that can comparable
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("Start Quarter From {:?}!", state);
+            25
+        },
+    }
+}
+
 fn main() {
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Quarter(UsState::Alabama));
     println!("Hello, world!");
 }
