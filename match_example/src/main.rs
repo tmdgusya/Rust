963e1994728f074dac032dae66471e096794c1b5
roach <tmdgusya@naver.com>
2021-08-16 23:26:48 +0900

    match 를 활용한 Option 활용

--- match_example/src/main.rs
+++ match_example/src/main.rs
@@ -27,9 +27,21 @@ fn value_in_cents(coin: Coin) -> u32 {
     }
 }
 
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None.
+        Some(i) => Some(i+1),
+    }
+}
+
 fn main() {
     value_in_cents(Coin::Penny);
     value_in_cents(Coin::Nickel);
     value_in_cents(Coin::Quarter(UsState::Alabama));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
     println!("Hello, world!");
 }
